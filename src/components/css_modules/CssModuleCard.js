552a0bbd0af0b0f98f83a222114add49ac60334c
baymarket96 <baymarkket96@gmail.com>
2022-05-28 20:35:13 -0700

    green titles

--- src/components/css_modules/CssModuleCard.js
+++ src/components/css_modules/CssModuleCard.js
@@ -9,7 +9,11 @@ const CssModuleCard = () => {
         I'm using <b>CSS modules</b>. I should be <b>BLUE</b>. My class names
         are <code>{"{styles.card}"}</code> and <code>{"{styles.title}"}</code>.
         In the browser, these class names will autogenerate with random
-        characters to avoid naming conflicts.
+        characters to avoid naming conflicts. Note that selectors like{" "}
+        <code>h2</code> that aren't nested affect other <code>h2</code>{" "}
+        elements. (The stylesheet imported only in this component has a rule to
+        make <code>h2</code> headings green. However, other cards also have a
+        green title. Remember that CSS modules solve just class name conflicts).
       </p>
     </div>
   );
